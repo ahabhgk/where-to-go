4c7829f38f6a6ae170681ab962abc0bfaba50ae9
ahabhgk <ahabhgk@gmail.com>
2019-06-10 21:18:38 +0800

    start Icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,21 @@
   <div class="home">
     <Header />
     <Carousel />
+    <Icons />
   </div>
 </template>
 
 <script>
 import Header from '@/components/layout/Header.vue'
 import Carousel from '@/components/Carousel.vue'
+import Icons from '@/components/Icons.vue'
 
 export default {
   name: 'home',
   components: {
     Header,
     Carousel,
+    Icons,
   },
 }
 </script>
