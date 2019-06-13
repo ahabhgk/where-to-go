fd1896a4e67a7238bfe1f16855a824ba64c306bc
ahabhgk <ahabhgk@gmail.com>
2019-06-13 14:25:09 +0800

    feat: city-search

---
+++ src/components/Search.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="search">
+    <input type="text" placeholder="输入城市名或拼音" />  
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'Search',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/style/variable.scss';
+
+.search {
+  height: $headerHeight;
+  padding: 0 1vw;
+  background: $bgColor;
+  input {
+    width: 94vw;
+    line-height: 6.5vw;
+    padding: 2vw;
+    border-radius: 1vw;
+    font-size: 16px;
+    text-align: center;
+  }
+}
+</style>
--- src/views/City.vue
+++ src/views/City.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <CityHeader />
+    <CitySearch />
   </div>
 </template>
 
 <script>
 import CityHeader from '@/components/CityHeader.vue'
+import CitySearch from '@/components/Search.vue'
 
 export default {
   name: 'City',
   components: {
     CityHeader,
+    CitySearch,
   },
 }
 </script>
